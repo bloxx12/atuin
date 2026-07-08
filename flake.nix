45c010675fa8f40ddb1cd993b7ec7d47fcc882a7
faukah <fau@faukah.com>
2026-07-08 20:12:26 +0200

    flake.nix: drop x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
         "aarch64-darwin"
       ];
       forEachSystem = nixpkgs.lib.genAttrs systems;
